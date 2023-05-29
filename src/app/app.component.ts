9697b53c119867e11ff989eefb5f6126cc0529bb
Nils Tijtgat <nilstijtgat@gmail.com>
2023-05-29 15:22:40 +0200

    Add Tauri and implement new features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatSelectChange } from '@angular/material/select';
 
 @Component({
     selector: 'app-root',
@@ -9,12 +11,14 @@ export class AppComponent {
     readonly expressionPedalMinValue = 70;
     readonly expressionPedalMaxValue = 600;
 
-    midiChannel = 1;
     lastButtonBits: number = 0;
     lastExpressionPedalValue: number = 0;
 
     midiOutputDevices: WebMidi.MIDIOutput[] = [];
     midiOutputDevice: WebMidi.MIDIOutput | null = null;
+
+    outputDeviceControl = new FormControl();
+    midiChannelControl = new FormControl<number>(1);
 
     constructor() {
         navigator.requestMIDIAccess().then((access) => {
@@ -23,8 +27,15 @@ export class AppComponent {
             if (midiOutputDevices.length > 0) {
                 this.midiOutputDevices = midiOutputDevices;
                 this.midiOutputDevice = midiOutputDevices[1];
+                this.outputDeviceControl.setValue(this.midiOutputDevice);
             }
         });
+    }
+
+    selectOutputDevice(event: MatSelectChange) {
+        if (event.value) {
+            this.midiOutputDevice = event.value;
+        }
     }
 
     convertExpressionPedalValue(value: number) {
@@ -44,9 +55,17 @@ export class AppComponent {
     }
 
     async getDevices() {
-        const device = await navigator.usb.requestDevice({
-            filters: [{ vendorId: 0x17cc }],
-        });
+        const device = await navigator.usb
+            .requestDevice({
+                filters: [{ vendorId: 0x17cc }],
+            })
+            .catch(() => {
+                // TO-DO: handle errors
+            });
+
+        if (!device) {
+            return;
+        }
 
         await device.open();
         await device.selectConfiguration(1);
@@ -55,7 +74,7 @@ export class AppComponent {
         while (true) {
             const result = await device.transferIn(1, 128);
 
-            if (result.data && this.midiOutputDevice) {
+            if (result.data && this.midiOutputDevice && this.midiChannelControl.value) {
                 if (result.data.byteLength === 8) {
                     const buttonBits = result.data.getInt8(1);
 
@@ -63,9 +82,9 @@ export class AppComponent {
                     for (let i = 0; i < 8; i++) {
                         if (this.isBitEnabled(this.lastButtonBits, i) !== this.isBitEnabled(buttonBits, i)) {
                             if (Boolean(buttonBits >> i)) {
-                                this.midiOutputDevice.send([144 + this.midiChannel - 1, i, 127]);
+                                this.midiOutputDevice.send([144 + this.midiChannelControl.value - 1, i, 127]);
                             } else {
-                                this.midiOutputDevice.send([144 + this.midiChannel - 1, i, 0]);
+                                this.midiOutputDevice.send([144 + this.midiChannelControl.value - 1, i, 0]);
                             }
                         }
                     }
@@ -77,7 +96,7 @@ export class AppComponent {
                     const expressionPedalValue = result.data.getInt16(5);
                     const mappedExpressionPedalValue = this.convertExpressionPedalValue(expressionPedalValue);
                     if (mappedExpressionPedalValue !== this.lastExpressionPedalValue) {
-                        this.midiOutputDevice.send([176 + this.midiChannel - 1, 1, mappedExpressionPedalValue]);
+                        this.midiOutputDevice.send([176 + this.midiChannelControl.value - 1, 1, mappedExpressionPedalValue]);
                         this.lastExpressionPedalValue = mappedExpressionPedalValue;
                     }
                 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        ReactiveFormsModule,
+    ],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
