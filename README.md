51ba6d2ab9b68634526032eec2e2fe218a18aab0
Nils Tijtgat <nilstijtgat@gmail.com>
2023-05-29 14:21:33 +0200

    Fixes

--- README.md
+++ README.md
@@ -1,27 +1,3 @@
-# RigKontrol2
+# Rig-Kontrol-Web
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.7.
-
-## Development server
-
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
-
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+This projects offers a browser based driver for Native Instrument's Rig Kontrol 3 hardware controller. As Native Instruments no longer supports this device and the drivers can't be installed on Windows 11 or latest macOS versions, these devices are technically rendered obsolete. The deployed webpage of this repository has an in-browser USB driver that reads from Rig Kontrol 3 and maps button and expression controller commands to a MIDI output device. This in turn allows you to then map those MIDI commands to whatever software you are using and would like to control.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,87 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
-  title = 'rig-kontrol2';
+    readonly expressionPedalMinValue = 70;
+    readonly expressionPedalMaxValue = 600;
+
+    midiChannel = 1;
+    lastButtonBits: number = 0;
+    lastExpressionPedalValue: number = 0;
+
+    midiOutputDevices: WebMidi.MIDIOutput[] = [];
+    midiOutputDevice: WebMidi.MIDIOutput | null = null;
+
+    constructor() {
+        navigator.requestMIDIAccess().then((access) => {
+            const midiOutputDevices = Array.from(access.outputs.values());
+
+            if (midiOutputDevices.length > 0) {
+                this.midiOutputDevices = midiOutputDevices;
+                this.midiOutputDevice = midiOutputDevices[1];
+            }
+        });
+    }
+
+    convertExpressionPedalValue(value: number) {
+        return Math.round(
+            Math.max(
+                Math.min(
+                    ((value - this.expressionPedalMinValue) * 127) / (this.expressionPedalMaxValue - this.expressionPedalMinValue),
+                    127
+                ),
+                0
+            )
+        );
+    }
+
+    isBitEnabled(input: number, bit: number) {
+        return (input >> bit) % 2 != 0;
+    }
+
+    async getDevices() {
+        const device = await navigator.usb.requestDevice({
+            filters: [{ vendorId: 0x17cc }],
+        });
+
+        await device.open();
+        await device.selectConfiguration(1);
+        await device.claimInterface(0);
+
+        while (true) {
+            const result = await device.transferIn(1, 128);
+
+            if (result.data && this.midiOutputDevice) {
+                if (result.data.byteLength === 8) {
+                    const buttonBits = result.data.getInt8(1);
+
+                    // Update switch values
+                    for (let i = 0; i < 8; i++) {
+                        if (this.isBitEnabled(this.lastButtonBits, i) !== this.isBitEnabled(buttonBits, i)) {
+                            if (Boolean(buttonBits >> i)) {
+                                this.midiOutputDevice.send([144 + this.midiChannel - 1, i, 127]);
+                            } else {
+                                this.midiOutputDevice.send([144 + this.midiChannel - 1, i, 0]);
+                            }
+                        }
+                    }
+                    this.lastButtonBits = buttonBits;
+                }
+
+                // Update expression pedal value
+                if (result.data.byteLength === 33) {
+                    const expressionPedalValue = result.data.getInt16(5);
+                    const mappedExpressionPedalValue = this.convertExpressionPedalValue(expressionPedalValue);
+                    if (mappedExpressionPedalValue !== this.lastExpressionPedalValue) {
+                        this.midiOutputDevice.send([176 + this.midiChannel - 1, 1, mappedExpressionPedalValue]);
+                        this.lastExpressionPedalValue = mappedExpressionPedalValue;
+                    }
+                }
+            }
+        }
+    }
 }
